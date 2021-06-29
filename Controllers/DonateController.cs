de3b81009b6da9f17b9c797fa366ebcfbac0c2c1
Daniel Habenicht <daniel-habenicht@outlook.de>
2021-06-29 02:13:24 +0200

    add first draft

---
+++ Controllers/DonateController.cs
@@ -0,0 +1,39 @@
+﻿using DataDonation.Database;
+using DataDonation.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DataDonation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DonateController : ControllerBase
+    {
+        private readonly ILogger<DonateController> _logger;
+        private readonly DatabaseContext _dbcontext;
+
+        public DonateController(ILogger<DonateController> logger, DatabaseContext dbcontext)
+        {
+            _logger = logger;
+            _dbcontext = dbcontext;
+        }
+
+        /// <summary>
+        /// Submits a data Donation
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult> Submit(JsonElement data)
+        {
+            // TODO: Add validation
+            var dataDonation = new DataDonationEntry(
+                Guid.NewGuid(), DateTime.Now, JsonSerializer.Serialize(data));
+            await _dbcontext.DataDonationEntries.AddAsync(dataDonation);
+            await _dbcontext.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ Migrations/20210629000238_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataDonation.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DataDonationEntries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    data = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataDonationEntries", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataDonationEntries");
+        }
+    }
+}
