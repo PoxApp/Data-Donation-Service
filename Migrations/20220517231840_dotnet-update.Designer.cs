bb68b81a0fe5af724becc36210e8ff0a39bffebd
Restyled.io <commits@restyled.io>
2022-05-17 23:24:15 +0000

    Restyled by clang-format

--- Migrations/20220517231840_dotnet-update.Designer.cs
+++ Migrations/20220517231840_dotnet-update.Designer.cs
@@ -8,36 +8,28 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace DataDonation.Migrations
-{
-[DbContext(typeof(DatabaseContext))]
-[Migration("20220517231840_dotnet-update")]
-partial class dotnetupdate
-{
-    protected override void BuildTargetModel(ModelBuilder modelBuilder)
-    {
+namespace DataDonation.Migrations {
+  [DbContext(typeof(DatabaseContext))]
+  [Migration("20220517231840_dotnet-update")]
+  partial class dotnetupdate {
+    protected override void BuildTargetModel(ModelBuilder modelBuilder) {
 #pragma warning disable 612, 618
-        modelBuilder
-        .HasAnnotation("ProductVersion", "6.0.5")
-        .HasAnnotation("Relational:MaxIdentifierLength", 64);
+      modelBuilder.HasAnnotation("ProductVersion", "6.0.5")
+          .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
-        modelBuilder.Entity("DataDonation.Models.DataDonationEntry", b =>
-        {
-            b.Property<Guid>("Id")
-            .ValueGeneratedOnAdd()
-            .HasColumnType("binary(16)");
+      modelBuilder.Entity("DataDonation.Models.DataDonationEntry", b => {
+        b.Property<Guid>("Id").ValueGeneratedOnAdd().HasColumnType(
+            "binary(16)");
 
-            b.Property<string>("data")
-            .HasColumnType("longtext");
+        b.Property<string>("data").HasColumnType("longtext");
 
-            b.Property<DateTime>("date")
-            .HasColumnType("datetime(6)");
+        b.Property<DateTime>("date").HasColumnType("datetime(6)");
 
-            b.HasKey("Id");
+        b.HasKey("Id");
 
-            b.ToTable("DataDonationEntries");
-        });
+        b.ToTable("DataDonationEntries");
+      });
 #pragma warning restore 612, 618
     }
-}
+  }
 }
