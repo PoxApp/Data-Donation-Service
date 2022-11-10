5a8eba6eaaea4d53d50372670a12fc93ce37bc93
DanielHabenicht <daniel-habenicht@outlook.de>
2022-11-10 20:25:13 +0000

    fix: remove connectionString logging

--- api/Startup.cs
+++ api/Startup.cs
@@ -8,58 +8,66 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
-namespace API {
-public class Startup {
-  public Startup(IConfiguration configuration) {
-    Configuration = configuration;
-  }
-
-  public IConfiguration Configuration { get; }
-  
-        // This method gets called by the runtime. Use this method to add services to
-  // the container.
-  public void ConfigureServices(IServiceCollection services) {
-    var connectionString =
-        Configuration.GetConnectionString("DefaultConnection");
-    Serilog.Log.Information(connectionString);
-    var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
-
-
-    try
+namespace API
+{
+    public class Startup
     {
-        serverVersion =
-            new MySqlServerVersion(ServerVersion.AutoDetect(connectionString));
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to
+        // the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString =
+                Configuration.GetConnectionString("DefaultConnection");
+            // Serilog.Log.Information(connectionString);
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
+
+
+            try
+            {
+                serverVersion =
+                    new MySqlServerVersion(ServerVersion.AutoDetect(connectionString));
+
+            }
+            catch (Exception)
+            {
+                // do nothing, it will crash later...
+            }
+            services.AddDbContext<DatabaseContext>(
+                options => options.UseMySql(connectionString, serverVersion));
+
+            services.AddControllers(); services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                             new OpenApiInfo { Title = "DataDonation", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the
+        // HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                                                        "DataDonation v1"));
+            }
+
+            // app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
     }
-    catch (Exception) { 
-        // do nothing, it will crash later...
-    }
-    services.AddDbContext<DatabaseContext>(
-        options => options.UseMySql(connectionString, serverVersion));
-
-    services.AddControllers(); services.AddSwaggerGen(c => {
-      c.SwaggerDoc("v1",
-                   new OpenApiInfo { Title = "DataDonation", Version = "v1" });
-    });
-  }
-
-  // This method gets called by the runtime. Use this method to configure the
-  // HTTP request pipeline.
-  public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-    if (env.IsDevelopment()) {
-      app.UseDeveloperExceptionPage();
-      app.UseSwagger();
-      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json",
-                                              "DataDonation v1"));
-    }
-
-    // app.UseHttpsRedirection();
-
-    app.UseRouting();
-
-    app.UseAuthorization();
-
-    app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
-  }
-}
 }
